5f16eb99cbae9cb126b0b28db31bf8b15efed85a
Stas Pakhomov <stas@Stass-MacBook-Air.local>
2025-11-10 19:46:18 -0800

    cleaning branch

--- engine/utils.js
+++
@@ -1,7 +0,0 @@
-export const toTitleCase = (str) =>
-  str
-    .replace(/[_\-]+/g, " ") // replace underscores/dashes
-    .replace(
-      /\w\S*/g,
-      (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()
-    );
